b2f714b6b34d661c5632ff516694181d72f28f65
s-alad <n7.saad@gmail.com>
2024-04-03 13:19:03 -0400

    add proc

--- app/main.py
+++ app/main.py
@@ -48,6 +48,6 @@ async def set_repo_users(request: Request):
 # ======================================== run the app =========================================
     
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
 
 # ==============================================================================================
